431f846b2d79c328167bf0b30d547f1910b5c58c
Rodrigo Demetrio <rodrigo.demetrio.palma@gmail.com>
2019-04-26 12:13:13 -0300

    tela de cadastro pessoal

--- lib/src/view/CadPessoaScreen.dart
+++ lib/src/view/CadPessoaScreen.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import '../util/MyFunctions.dart';
 import 'Widgets.dart';
+import '../util/colors.dart';
 
 class CadPessoaScreen extends StatelessWidget {
   MediaQueryData queryData;
@@ -14,45 +17,25 @@ class CadPessoaScreen extends StatelessWidget {
         title: Text("Cadastro Pessoal"),
       ),
       body: SingleChildScrollView(
-              child: Column(
+        child: Column(
           children: <Widget>[
             Padding(
               padding:
                   EdgeInsets.fromLTRB(0, Dp2Pixel(16, devicePixelRatio), 0, 0),
             ),
-            Center(
-              child: Text("As informações preenchidas serão divulgadas"),
-            ),
-            Center(
-              child: Text("apenas para a pessoa com a qual você realizar"),
-            ),
-            Center(
-              child: Text("o processo de adoção e/ou apadrinhamento,"),
-            ),
-            Center(
-              child: Text("após a formalização do processo."),
-            ),
             Padding(
-              padding: EdgeInsets.fromLTRB(0, Dp2Pixel(28, devicePixelRatio), 0,
-                  Dp2Pixel(32, devicePixelRatio)),
-              child: Text("INFORMAÇÕES PESSOAIS"),
+              padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+              child: Container(
+                color: primaryColor,
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("As informações preenchidas serão divulgadas\n apenas para a pessoa com a qual você realizar\n o processo de adoção e/ou apadrinhamento,\n após a formalização do processo."),
+                  ),
+                ),
+              ),
             ),
             MyCustomForm(),
-            Padding(
-              padding: EdgeInsets.fromLTRB(0, Dp2Pixel(28, devicePixelRatio), 0,
-                  Dp2Pixel(32, devicePixelRatio)),
-              child: Text("INFORMAÇÕES DE PERFIL"),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(0, Dp2Pixel(28, devicePixelRatio), 0,
-                  Dp2Pixel(32, devicePixelRatio)),
-              child: Text("FOTO DE PERFIL"),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(0, Dp2Pixel(32, devicePixelRatio), 0,
-                  Dp2Pixel(24, devicePixelRatio)),
-              child: ButtonWidget(text: "Fazer Cadastro", rota: ''),
-            ),
           ],
         ),
       ),
@@ -71,6 +54,7 @@ class MyCustomForm extends StatefulWidget {
 // Create a corresponding State class. This class will hold the data related to
 // the form.
 class MyCustomFormState extends State<MyCustomForm> {
+  MediaQueryData queryData;
   // Create a global key that will uniquely identify the Form widget and allow
   // us to validate the form
   //
@@ -79,6 +63,8 @@ class MyCustomFormState extends State<MyCustomForm> {
 
   @override
   Widget build(BuildContext context) {
+    queryData = MediaQuery.of(context);
+    double devicePixelRatio = queryData.devicePixelRatio;
     // Build a Form widget using the _formKey we created above
     return Form(
       key: _formKey,
@@ -86,11 +72,14 @@ class MyCustomFormState extends State<MyCustomForm> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Padding(
+            padding:
+                EdgeInsets.fromLTRB(20, 30, 0, Dp2Pixel(32, devicePixelRatio)),
+            child: Text("INFORMAÇÕES PESSOAIS", style: TextStyle(color: primaryColor),),
+          ),
+          Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextFormField(
-              decoration: InputDecoration(
-                hintText: "Nome:"
-              ),
+              decoration: InputDecoration(hintText: "Nome completo:"),
               validator: (value) {
                 if (value.isEmpty) {
                   return 'Please enter some text';
@@ -101,9 +90,7 @@ class MyCustomFormState extends State<MyCustomForm> {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextFormField(
-              decoration: InputDecoration(
-                hintText: "Idade:"
-              ),
+              decoration: InputDecoration(hintText: "Idade:"),
               validator: (value) {
                 if (value.isEmpty) {
                   return 'Please enter some text';
@@ -114,9 +101,7 @@ class MyCustomFormState extends State<MyCustomForm> {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextFormField(
-              decoration: InputDecoration(
-                hintText: "E-mail:"
-              ),
+              decoration: InputDecoration(hintText: "E-mail:"),
               validator: (value) {
                 if (value.isEmpty) {
                   return 'Please enter some text';
@@ -127,9 +112,7 @@ class MyCustomFormState extends State<MyCustomForm> {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextFormField(
-              decoration: InputDecoration(
-                hintText: "Estado:"
-              ),
+              decoration: InputDecoration(hintText: "Estado:"),
               validator: (value) {
                 if (value.isEmpty) {
                   return 'Please enter some text';
@@ -140,9 +123,7 @@ class MyCustomFormState extends State<MyCustomForm> {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextFormField(
-              decoration: InputDecoration(
-                hintText: "Endereço:"
-              ),
+              decoration: InputDecoration(hintText: "Endereço:"),
               validator: (value) {
                 if (value.isEmpty) {
                   return 'Please enter some text';
@@ -153,9 +134,7 @@ class MyCustomFormState extends State<MyCustomForm> {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextFormField(
-              decoration: InputDecoration(
-                hintText: "Telefone:"
-              ),
+              decoration: InputDecoration(hintText: "Telefone:"),
               validator: (value) {
                 if (value.isEmpty) {
                   return 'Please enter some text';
@@ -164,18 +143,67 @@ class MyCustomFormState extends State<MyCustomForm> {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: RaisedButton(
-              onPressed: () {
-                // Validate will return true if the form is valid, or false if
-                // the form is invalid.
-                if (_formKey.currentState.validate()) {
-                  // If the form is valid, we want to show a Snackbar
-                  Scaffold.of(context)
-                      .showSnackBar(SnackBar(content: Text('Processing Data')));
+            padding:
+                EdgeInsets.fromLTRB(20, 30, 0, Dp2Pixel(32, devicePixelRatio)),
+            child: Text("INFORMAÇÕES DE PERFIL", style: TextStyle(color: primaryColor),),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              decoration: InputDecoration(hintText: "Nome de Usuário:"),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter some text';
                 }
               },
-              child: Text('Submit'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              decoration: InputDecoration(hintText: "Senha:"),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter some text';
+                }
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              decoration: InputDecoration(hintText: "Confirmação de senha:"),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter some text';
+                }
+              },
+            ),
+          ),
+          Padding(
+            padding:
+                EdgeInsets.fromLTRB(20, 30, 0, Dp2Pixel(32, devicePixelRatio)),
+            child: Text("FOTO DE PERFIL", style: TextStyle(color: primaryColor),),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Center(
+              child: SizedBox(
+                width: 200,
+                child: RaisedButton(
+                  color: primaryColor,
+                  onPressed: () {
+                    // Validate will return true if the form is valid, or false if
+                    // the form is invalid.
+                    if (_formKey.currentState.validate()) {
+                      // If the form is valid, we want to show a Snackbar
+                      Scaffold.of(context).showSnackBar(
+                          SnackBar(content: Text('Processing Data')));
+                    }
+                  },
+                  child: Text('Fazer Cadastro'),
+                ),
+              ),
             ),
           ),
         ],
