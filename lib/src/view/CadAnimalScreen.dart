a907ea67062fbf076b6af0d07fc090ebeae60f78
Rodrigo Demetrio <rodrigo.demetrio.palma@gmail.com>
2019-05-03 11:11:42 -0300

    cad_animal

--- lib/src/view/CadAnimalScreen.dart
+++ lib/src/view/CadAnimalScreen.dart
@@ -10,6 +10,45 @@ class CadAnimalScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     queryData = MediaQuery.of(context);
     double devicePixelRatio = queryData.devicePixelRatio;
+    if (1==1) {
+      return Scaffold(
+      appBar: AppBar(
+        title: Text("Cadastro"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, Dp2Pixel(52, devicePixelRatio), 0,
+                Dp2Pixel(52, devicePixelRatio)),
+            child: Center(
+                child: Text(
+              "Ops",
+              style: TextStyle(fontSize: 72, color: primaryLightColor),
+            )),
+          ),
+          Center(
+            child: Text("Você não pode realizar esta ação sem"),
+          ),
+          Center(
+            child: Text("possuir um cadastro"),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, Dp2Pixel(16, devicePixelRatio), 0,
+                Dp2Pixel(44, devicePixelRatio)),
+            child: ButtonWidget(text: "Fazer cadastro", rota: '/cad_pessoa'),
+          ),
+          Center(
+            child: Text("Já possui cadastro?"),
+          ),
+          Padding(
+            padding:
+                EdgeInsets.fromLTRB(0, Dp2Pixel(16, devicePixelRatio), 0, 0),
+            child: ButtonWidget(text: "Fazer login", rota: '/main'),
+          ),
+        ],
+      ),
+    );
+    } else {
     return Scaffold(
       appBar: AppBar(
         title: Text("Cadastro"),
@@ -48,4 +87,5 @@ class CadAnimalScreen extends StatelessWidget {
       ),
     );
   }
+}
 }
