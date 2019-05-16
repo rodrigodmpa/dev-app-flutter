78c43aeab47e484d1622644ff4b1b294ab097116
MathCastro <matheusfk74@gmail.com>
2019-05-16 17:36:23 -0300

    SignOut and image firebase

--- lib/src/controller/auth/AuthController.dart
+++ lib/src/controller/auth/AuthController.dart
@@ -1,17 +1,21 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:myapp/src/model/User.dart';
 
 class AuthController {
-
   final reference = FirebaseDatabase.instance.reference();
   final FirebaseAuth _auth = FirebaseAuth.instance;
   static bool logged = false;
   static User loggedUser;
 
   void registerUser(User user) async {
-    final String id = await signUpWithEmailAndPassword(user.email, user.password); 
-    final userReference = FirebaseDatabase.instance.reference().child("user").child(id);
+    final String id =
+        await signUpWithEmailAndPassword(user.email, user.password);
+    final userReference =
+        FirebaseDatabase.instance.reference().child("user").child(id);
 
     var newUser = <String, dynamic>{
       'name': user.name,
@@ -28,8 +32,10 @@ class AuthController {
     logged = true;
   }
 
-  Future<String> signUpWithEmailAndPassword(String email, String password) async {
-    final FirebaseUser userFirebase = await _auth.createUserWithEmailAndPassword(
+  Future<String> signUpWithEmailAndPassword(
+      String email, String password) async {
+    final FirebaseUser userFirebase =
+        await _auth.createUserWithEmailAndPassword(
       email: email,
       password: password,
     );
@@ -67,30 +73,30 @@ class AuthController {
       },
     );
 
-    user = User(address: maps["address"], email: maps["email"], id: id, idade: maps["idade"], name: maps["name"], password: maps["password"], phone: maps["phone"], state: maps["state"], userName: maps["userName"]);
+    user = User(
+        address: maps["address"],
+        email: maps["email"],
+        id: id,
+        idade: maps["idade"],
+        name: maps["name"],
+        password: maps["password"],
+        phone: maps["phone"],
+        state: maps["state"],
+        userName: maps["userName"]);
     return user;
   }
 
   Future<void> signOut() async {
     logged = false;
+    loggedUser = null;
     return _auth.signOut();
   }
 
-  
-  // Future<FirebaseUser> _handleSignIn() async {
-  //   final GoogleSignIn _googleSignIn = GoogleSignIn();
-  //   final FirebaseAuth _auth = FirebaseAuth.instance;
-  //   final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
-  //   final GoogleSignInAuthentication googleAuth =
-  //       await googleUser.authentication;
-
-  //   final AuthCredential credential = GoogleAuthProvider.getCredential(
-  //     accessToken: googleAuth.accessToken,
-  //     idToken: googleAuth.idToken,
-  //   );
-
-  //   final FirebaseUser user = await _auth.signInWithCredential(credential);
-  //   print("signed in " + user.displayName);
-  //   return user;
-  // }
+  Future<String> _pickSaveImage(String imageId, File imageFile) async {
+    final String id = await AuthController().getCurrentUser();
+    StorageReference ref =
+        FirebaseStorage.instance.ref().child(id).child("image.jpg");
+    StorageUploadTask uploadTask = ref.putFile(imageFile);
+    return await (await uploadTask.onComplete).ref.getDownloadURL();
+  }
 }
